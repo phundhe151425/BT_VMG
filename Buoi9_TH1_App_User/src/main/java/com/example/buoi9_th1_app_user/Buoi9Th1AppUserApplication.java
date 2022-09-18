559f4661a3bd84ec4114f1bd53ab0391ce988328
Nguyen Dinh Phu <phunguyen06072001@gmail.com>
2022-09-18 12:57:43 +0700

    .

---
+++ Buoi9_TH1_App_User/src/main/java/com/example/buoi9_th1_app_user/Buoi9Th1AppUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.buoi9_th1_app_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buoi9Th1AppUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Buoi9Th1AppUserApplication.class, args);
+    }
+
+}
---
+++ Buoi9_TH1_App_User/src/test/java/com/example/buoi9_th1_app_user/Buoi9Th1AppUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buoi9_th1_app_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buoi9Th1AppUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
